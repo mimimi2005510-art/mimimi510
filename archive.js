2a0b8cb28f35fea979041bd8c6f0740d0bd7a7d0
mimimi2005510-art <mimimi2005510@gmail.com>
2026-07-26 11:18:45 +0900

    Update archive.js

--- archive.js
+++ archive.js
@@ -1,1 +1,22 @@
+const openBtn =
+document.getElementById("openIllustrationLibrary");
 
+const library =
+document.getElementById("illustrationLibrary");
+
+const closeBtn =
+document.getElementById("closeLibrary");
+
+openBtn.onclick=()=>{
+    library.style.display="flex";
+}
+
+closeBtn.onclick=()=>{
+    library.style.display="none";
+}
+
+library.onclick=(e)=>{
+    if(e.target===library){
+        library.style.display="none";
+    }
+}
